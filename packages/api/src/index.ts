fbc0684b9dd6fd8d7be03f0cc23f0294cdd6dfb8
epaew <epaew.333@gmail.com>
2020-02-15 14:37:35 +0900

    Update and merge tsconfig.json

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,8 +1,7 @@
 import 'reflect-metadata'
 import { createConnection } from 'typeorm'
-import * as express from 'express'
 import * as bodyParser from 'body-parser'
-import { Request, Response } from 'express'
+import express, { Request, Response } from 'express'
 import { Routes } from './routes'
 import { User } from './entity/User'
 
