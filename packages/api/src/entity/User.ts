6f2dbd447ae900f23cb7185db14221636679848d
epaew <epaew.333@gmail.com>
2020-02-15 18:20:29 +0900

    Update user entity

--- packages/api/src/entity/User.ts
+++ packages/api/src/entity/User.ts
@@ -1,16 +1,16 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm'
 
-@Entity()
-export class User {
-    @PrimaryGeneratedColumn()
-    id!: number;
+@Entity('users')
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id!: number
 
-    @Column()
-    firstName!: string;
+  @Column()
+  name!: string
 
-    @Column()
-    lastName!: string;
+  @CreateDateColumn({ name: 'created_at', type: 'timestamp with time zone' })
+  createdAt!: Date
 
-    @Column()
-    age!: number;
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamp with time zone' })
+  updatedAt!: Date
 }
