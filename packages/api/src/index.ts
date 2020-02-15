d56817b80a74f5cf3f42c1a73762298fe0280678
epaew <epaew.333@gmail.com>
2020-02-15 18:06:17 +0900

    Add apollo-server-express

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,44 +1,25 @@
 import 'reflect-metadata'
+import { ApolloServer, gql } from 'apollo-server-express'
 import { createConnection } from 'typeorm'
-import * as bodyParser from 'body-parser'
-import express, { Request, Response } from 'express'
-import { Routes } from './routes'
-import { User } from './entity/User'
+import express from 'express'
 
-createConnection().then(async connection => {
+createConnection().then(async () => {
+  const typeDefs = gql`
+    type Query {
+      hello: String
+    }
+  `
+  const resolvers = {
+    Query: { hello: () => 'Hello world!' }
+  }
+  const apolloServer = new ApolloServer({ typeDefs, resolvers })
+
   // create express app
   const app = express()
-  app.use(bodyParser.json())
-
-  // register express routes from defined application routes
-  Routes.forEach(route => {
-    (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-      const result = (new (route.controller as any)())[route.action](req, res, next)
-      if (result instanceof Promise) {
-        result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
-      } else if (result !== null && result !== undefined) {
-        res.json(result)
-      }
-    })
-  })
-
-  // setup express app here
-  // ...
+  apolloServer.applyMiddleware({ app })
 
   // start express server
-  app.listen(3000)
-
-  // insert new users for test
-  await connection.manager.save(connection.manager.create(User, {
-    firstName: 'Timber',
-    lastName: 'Saw',
-    age: 27
-  }))
-  await connection.manager.save(connection.manager.create(User, {
-    firstName: 'Phantom',
-    lastName: 'Assassin',
-    age: 24
-  }))
-
-  console.log('Express server has started on port 3000. Open http://localhost:3000/users to see results')
+  app.listen(3000, () => {
+    console.log('Express server has started on port 3000.')
+  })
 }).catch(error => console.log(error))
