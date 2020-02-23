d7e6e95563fe20fb22da2d98359f1b09769aeb8f
epaew <epaew.333@gmail.com>
2020-02-23 11:04:18 +0900

    Update App.tsx to use apollo client

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { apolloClient } from './src/graphql'
+import { ApolloProvider } from '@apollo/react-hooks'
 import { StyleSheet, Text, View } from 'react-native'
 
 export default function App () {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <ApolloProvider client={apolloClient}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
+    </ApolloProvider>
   )
 }
 
