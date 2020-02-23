976e2e72490fbf9d6c700160f459215fa89d23aa
epaew <epaew.333@gmail.com>
2020-02-23 12:10:40 +0900

    Update App.tsx to execute query and display result (test)

--- app/App.tsx
+++ app/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
-import { apolloClient } from './src/graphql'
+import { apolloClient, useGetChannelsQuery } from './src/graphql'
 import { ApolloProvider } from '@apollo/react-hooks'
 import { StyleSheet, Text, View } from 'react-native'
 
 export default function App () {
+  const { data, loading } = useGetChannelsQuery({
+    client: apolloClient
+  })
+
   return (
     <ApolloProvider client={apolloClient}>
       <View style={styles.container}>
         <Text>Open up App.tsx to start working on your app!</Text>
+        <Text>{!loading && JSON.stringify(data?.channels, null, 2)}</Text>
       </View>
     </ApolloProvider>
   )
