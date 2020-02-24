27285f50443dc1fb1babc89a4e0d898b287aa94d
epaew <epaew.333@gmail.com>
2020-02-24 13:17:46 +0900

    Move firebase sign-in/sign-out methods to lib

--- app/src/lib/firebase.ts
+++ app/src/lib/firebase.ts
@@ -3,6 +3,11 @@ import firebaseConfig from '../../firebase.json'
 
 firebase.initializeApp(firebaseConfig)
 
+const githubProvider = new firebase.auth.GithubAuthProvider()
+
 export const auth = firebase.auth()
 export type FirebaseUser = firebase.User | null
-export const githubProvider = new firebase.auth.GithubAuthProvider()
+export const signIn = {
+  withGithub: async () => auth.signInWithRedirect(githubProvider)
+}
+export const signOut = async () => auth.signOut()
