73ed64db0efb9843c683fbff6899f68369aac912
epaew <epaew.333@gmail.com>
2020-02-24 15:36:07 +0900

    Implement currentUser query in server side

--- api/src/graphql/resolvers/index.ts
+++ api/src/graphql/resolvers/index.ts
@@ -1,11 +1,12 @@
 import { Resolvers } from '../'
 
 import { createChannel, channels } from './Channel'
-import { createUser, user } from './User'
+import { createUser, currentUser, user } from './User'
 
 export const resolvers: Resolvers = {
   Query: {
     channels,
+    currentUser,
     user
   },
   Mutation: {
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata'
-import { ApolloServer } from 'apollo-server-express'
+import { ApolloServer, AuthenticationError } from 'apollo-server-express'
+import { auth } from './lib/firebaseAdmin'
 import { BaseEntity, createConnection } from 'typeorm'
 import express from 'express'
 import { typeDefs, resolvers } from './graphql'
@@ -9,7 +10,23 @@ const startApp = async () => {
   BaseEntity.useConnection(connection)
 
   const app = express()
-  const apolloServer = new ApolloServer({ typeDefs, resolvers })
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: async ({ req, res }) => {
+      const token = req.headers.authorization
+      if (!token) {
+        throw new AuthenticationError('Invalid token')
+      }
+
+      try {
+        const user = await auth.verifyIdToken(token.replace('Bearer ', ''))
+        return { req, res, user }
+      } catch {
+        throw new AuthenticationError('Invalid token')
+      }
+    }
+  })
   const port = process.env.PORT || 3000
   apolloServer.applyMiddleware({ app })
 
