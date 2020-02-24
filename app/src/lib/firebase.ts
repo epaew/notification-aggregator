542a1e56d59c198796a31595fa5a75660312866b
epaew <epaew.333@gmail.com>
2020-02-24 12:12:46 +0900

    Fixup firebase codes for Expo/iOS app, but still not working...

--- app/src/lib/firebase.ts
+++ app/src/lib/firebase.ts
@@ -1,5 +1,7 @@
+import { AuthSession } from 'expo'
 import firebase from 'firebase'
 import firebaseConfig from '../../firebase.json'
+import { Platform } from 'react-native'
 
 firebase.initializeApp(firebaseConfig)
 
@@ -9,6 +11,20 @@ export const auth = firebase.auth()
 export type FirebaseUser = firebase.User | null
 export const signIn = {
   asAnonymous: async () => auth.signInAnonymously(),
-  withGithub: async () => auth.signInWithRedirect(githubProvider)
+  withGithub: async () => {
+    if (Platform.OS === 'web') {
+      return auth.signInWithRedirect(githubProvider)
+    } else {
+      const returnUrl = AuthSession.getRedirectUrl()
+      const authUrl = `https://${firebaseConfig.authDomain}/__/auth/handler` +
+        '?authType=signInViaRedirect&providerId=github.com' +
+        `&apiKey=${firebaseConfig.apiKey}&v=${firebase.SDK_VERSION}` +
+        `&redirectUrl=${encodeURIComponent(returnUrl)}`
+      // await AuthSession.startAsync({ authUrl })
+      // const result = await auth.getRedirectResult()
+      const result = await AuthSession.startAsync({ authUrl })
+      console.log(JSON.stringify(result, null, 4))
+    }
+  }
 }
 export const signOut = async () => auth.signOut()
