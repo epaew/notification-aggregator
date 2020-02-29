f076a1c600f579451b01865fe8fd52c65f0960f9
epaew <epaew.333@gmail.com>
2020-02-29 13:02:06 +0900

    Update `currentUser` query resolver:
    Implicitly create user if user was not present in database

--- api/src/graphql/index.ts
+++ api/src/graphql/index.ts
@@ -9,7 +9,13 @@ export const typeDefs = gql`
   ${fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8')}
 `
 
-type ContextType = { user: { uid: string } }
+type ContextType = {
+  user: {
+    uid: string
+    name?: string
+    picture?: string
+  }
+}
 export type Resolvers = generated.Resolvers<ContextType>
 export type QueryResolvers = generated.QueryResolvers<ContextType>
 export type MutationResolvers = generated.MutationResolvers<ContextType>
--- api/src/graphql/resolvers/User.ts
+++ api/src/graphql/resolvers/User.ts
@@ -1,5 +1,16 @@
 import { QueryResolvers } from '../'
 import { User } from '../../entity'
 
-export const currentUser: QueryResolvers['currentUser'] = async (_parent, _args, { user }) => User.findOne({ id: user.uid })
+export const currentUser: QueryResolvers['currentUser'] = async (_parent, _args, context) => {
+  const { user: firebaseUser } = context
+  let user = await User.findOne({ id: firebaseUser.uid })
+  if (user) return user
+
+  user = Object.assign(new User(), {
+    id: firebaseUser.uid,
+    displayName: firebaseUser.name || 'Anonymous User',
+    photoURL: firebaseUser.picture
+  })
+  return user.save()
+}
 export const user: QueryResolvers['user'] = async (_parent, args) => User.findOne(args)
