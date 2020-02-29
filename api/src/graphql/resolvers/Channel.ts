f311c2a9f586f893951e293ee74a8cbd76f6097f
epaew <epaew.333@gmail.com>
2020-02-29 14:07:53 +0900

    Fixup `channels` query to use args

--- api/src/graphql/resolvers/Channel.ts
+++ api/src/graphql/resolvers/Channel.ts
@@ -1,10 +1,22 @@
+import { FindConditions, Like } from 'typeorm'
+
 import { Channel, User } from '../../entity'
 import { QueryResolvers, MutationResolvers } from '../'
+
+export const channels: QueryResolvers['channels'] = async (_parent, { name, offset, limit }) => {
+  const where: FindConditions<Channel> = { public: true }
+  if (name) where.name = Like(`%${name}%`)
+
+  return Channel.find({
+    where,
+    order: { name: 'ASC' },
+    skip: offset,
+    take: limit
+  })
+}
 
 export const createChannel: MutationResolvers['createChannel'] = async (_parent, args, { user }) => {
   const channel = Object.assign(new Channel(), args.input)
   channel.createdBy = await User.findOneOrFail({ id: user.uid })
   return channel.save()
 }
-
-export const channels: QueryResolvers['channels'] = async () => Channel.find()
