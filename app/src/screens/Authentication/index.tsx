c53896583f7e499dee38c33966cecc3f92ff7e22
epaew <epaew.333@gmail.com>
2020-03-07 15:14:48 +0900

    Update Header of StackNavigation

--- app/src/screens/Authentication/index.tsx
+++ app/src/screens/Authentication/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 
+import { createHeader } from '../../components/Header'
 import { SigninScreen } from './Signin'
 
 const Stack = createStackNavigator()
 
 export const AuthenticationStack: React.FC = () => {
   return (
-    <Stack.Navigator initialRouteName='Home'>
+    <Stack.Navigator initialRouteName='Signin' screenOptions={{ header: createHeader(false) }}>
       <Stack.Screen name='Signin' component={SigninScreen} />
     </Stack.Navigator>
   )
--- app/src/screens/Channels/index.tsx
+++ app/src/screens/Channels/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 
+import { createHeader } from '../../components/Header'
 import { ListChannelScreen } from './List'
 
 const Stack = createStackNavigator()
 
 export const ChannelStack: React.FC = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName='Channels' screenOptions={{ header: createHeader() }}>
       <Stack.Screen name='Channels' component={ListChannelScreen} />
       {/* <Stack.Screen name='CreateChannel' component={} /> */}
       {/* <Stack.Screen name='EditChannel' component={} /> */}
